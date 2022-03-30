37a6f2be942b8dbe9c0a272116e030f2f43aefe6
hwandev12 <husan.ahmedov777@yandex.com>
2022-03-30 23:19:23 +0500

    crm_edits

---
+++ crmblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrmblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crmblog'
--- syscrm/settings.py
+++ syscrm/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crmblog',
 ]
 
 MIDDLEWARE = [
@@ -42,7 +43,7 @@ ROOT_URLCONF = 'syscrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'frontend' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
