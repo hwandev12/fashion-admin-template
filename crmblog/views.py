49ad7e8798bee8e2447e0cd5c719f2fe2afe25f5
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-08 22:38:09 +0500

    mixins

--- crmblog/views.py
+++ crmblog/views.py
@@ -2,13 +2,14 @@ from multiprocessing import context
 from re import template
 from django.shortcuts import render, redirect, reverse
 from .models import Lead, Spy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import models
 from django.shortcuts import get_object_or_404
 from .forms import *
 
 
-class SignupView(CreateView):
+class SignupView(LoginRequiredMixin, CreateView):
     template_name = 'registration/signup.html'
     form_class = RegisterForm
     
@@ -19,25 +20,25 @@ class HomeView(TemplateView):
     template_name = 'base.html'
 
 
-class Leads(ListView):
+class Leads(LoginRequiredMixin, ListView):
     template_name = 'leads_info.html'
     queryset = models.Lead.objects.all()
     context_object_name = 'leads'
 
 
-class DetailsLead(DetailView):
+class DetailsLead(LoginRequiredMixin, DetailView):
     template_name = 'details.html'
     context_object_name = 'lead'
     queryset = models.Lead.objects.all()
     
-class CreateLead(CreateView):
+class CreateLead(LoginRequiredMixin, CreateView):
     template_name = 'create.html'
     form_class = LeadForm
     
     def get_success_url(self):
         return reverse('lead:leads')
     
-class UpdateLead(UpdateView):
+class UpdateLead(LoginRequiredMixin, UpdateView):
     template_name = 'update.html'
     form_class = LeadForm
     queryset = models.Lead.objects.all()
@@ -45,7 +46,7 @@ class UpdateLead(UpdateView):
     def get_success_url(self):
         return reverse('lead:leads')
 
-class DeleteLead(DeleteView):
+class DeleteLead(LoginRequiredMixin, DeleteView):
     template_name = 'delete.html'
     queryset = models.Lead.objects.all()
     
