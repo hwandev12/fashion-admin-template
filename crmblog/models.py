912827a6a374f8df9bcd81c5a6725e02b0d558c4
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-12 10:57:33 +0500

    filtering database and deleting

--- crmblog/models.py
+++ crmblog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 
 class User(AbstractUser):
     pass
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user.username)
 
 class Lead(models.Model):
     class Meta:
@@ -23,6 +29,14 @@ class Lead(models.Model):
 
 class Spy(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user)
+
+
+def create_post_save(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)    
+
+post_save.connect(create_post_save, sender=User)
