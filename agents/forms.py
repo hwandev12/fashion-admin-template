25e48a87f3469c3219a5530ec8744e87707023a9
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 21:25:35 +0500

    create agent form

--- agents/forms.py
+++ agents/forms.py
@@ -1,10 +1,16 @@
 from dataclasses import field
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from crmblog.models import Spy
 
+User = get_user_model()
 class AgentCreateModel(forms.ModelForm):
     class Meta:
-        model = Spy
+        model = User
         fields = (
-            'user',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
         )
