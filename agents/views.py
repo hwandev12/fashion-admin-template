d7e2101c9480edc77db4be5de50eb74a4474fff9
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 21:32:51 +0500

    create agent form

--- agents/views.py
+++ agents/views.py
@@ -1,5 +1,6 @@
-from re import template
+import random
 from django.shortcuts import render
+from django.core.mail import send_mail
 from django.views import generic
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -22,9 +23,21 @@ class AgentCreateView(OrganiserAndLoginRequiredMixin, generic.CreateView):
         return reverse('agents:agent-list')
     
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.profile = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_organised = False
+        user.is_agent = True
+        user.set_password(f"{random.randint(0, 10000)}")
+        user.save()
+        Spy.objects.create(
+            user=user,
+            organiser=self.request.user.userprofile
+        )
+        send_mail(
+            subject='Agent has been created',
+            message='Your New Agent has been created',
+            from_email='guli@gmail.com',
+            recipient_list=[user.email],
+        )
         return super(AgentCreateView, self).form_valid(form)
 class AgentDetailView(OrganiserAndLoginRequiredMixin, generic.DetailView):
     template_name = 'agents/details.html'
