8148e11b51b8d767b1446a99ace63e8357e175f4
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-17 12:42:44 +0500

    unassigned lists

--- crmblog/views.py
+++ crmblog/views.py
@@ -32,6 +32,21 @@ class Leads(LoginRequiredMixin, ListView):
             queryset = models.Lead.objects.filter(organiser=user.spy.organiser)
             queryset = queryset.filter(spy__user=self.request.user)
         return queryset
+    
+    # Bu yerda aniqlanmagan agent uchun funksiya yozilgan
+    def get_context_data(self, **kwargs): # Eslab qolish
+        context = super(Leads, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organised:
+            queryset = Lead.objects.filter(
+                organiser = user.userprofile,
+                spy__isnull = True
+            )
+            context.update({
+                'unassigned_leads': queryset
+            })
+            return context
+    # Bu yerda aniqlanmagan agent uchun funksiya yozilgan
 
 
 class DetailsLead(LoginRequiredMixin, DetailView):
