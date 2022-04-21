cc2e957956e538059c9aa6b0d30bd4ea1e00debc
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 21:26:17 +0500

    crete category url update

--- crmblog/urls.py
+++ crmblog/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("<int:pk>/update/", UpdateLead.as_view(), name='update_lead'),
     path("<int:pk>/delete/", DeleteLead.as_view(), name='delete_lead'),
     path("<int:pk>/agent-assign/", AgentAssignView.as_view(), name='assign_agent'),
+    path("<int:pk>/category-update/",
+         LeadCategoryUpdateView.as_view(), name='category_update'),
     path("create/", CreateLead.as_view(), name='create_lead'),
     path("category-assign/", CategoryAssignView.as_view(), name='category_lead'),
     path("category-assign/<int:pk>/",
