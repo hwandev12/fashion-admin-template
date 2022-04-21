27e054b00c5291e11480fef890ea41a8bed741e2
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 21:25:58 +0500

    create category update class view

--- crmblog/views.py
+++ crmblog/views.py
@@ -155,3 +155,21 @@ class AssignCategoryDetailsView(LoginRequiredMixin, DetailView):
             queryset = Category.objects.filter(
                 organiser=user.spy.organiser)
         return queryset
+
+
+class LeadCategoryUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'category_update.html'
+    form_class = LeadCategoryUpdateForm
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organised:
+            queryset = Lead.objects.filter(
+                organiser=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(
+                organiser=user.spy.organiser)
+        return queryset
+
+    def get_success_url(self):
+        return reverse('lead:leads')
