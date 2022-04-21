6e2dc880b5aa72560d3a80b25a2b506a19f1ddfa
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 07:35:44 +0500

    add Category to the admin

--- crmblog/admin.py
+++ crmblog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Lead, User, Spy, UserProfile
+from .models import Lead, User, Spy, UserProfile, Category
 
 admin.site.register(Lead)
 admin.site.register(User)
 admin.site.register(Spy)
 admin.site.register(UserProfile)
-
+admin.site.register(Category)
