ec30f39fe5de2fbb03142107d16d34c307cf0507
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 17:31:31 +0500

    create context data for unassign category

--- crmblog/views.py
+++ crmblog/views.py
@@ -117,6 +117,21 @@ class CategoryAssignView(LoginRequiredMixin, ListView):
     template_name = 'category.html'
     context_object_name = 'categories'
 
+    def get_context_data(self, **kwargs):  # Eslab qolish
+        context = super(CategoryAssignView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organised:
+            queryset = Lead.objects.filter(
+                organiser=user.userprofile
+            )
+        else:
+            queryset = Category.objects.filter(
+                organiser=user.spy.organiser)
+        context.update({
+            'unassigned_category_quantity': queryset.filter(category__isnull=True).count()
+        })
+        return context
+
     def get_queryset(self):
         user = self.request.user
         if user.is_organised:
