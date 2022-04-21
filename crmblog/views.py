d8bb0590d43c52f7be7b5dd0c39ac6d681367e38
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 20:43:27 +0500

    create category detail view

--- crmblog/views.py
+++ crmblog/views.py
@@ -141,3 +141,17 @@ class CategoryAssignView(LoginRequiredMixin, ListView):
             queryset = Category.objects.filter(
                 organiser=user.spy.organiser)
         return queryset
+
+
+class AssignCategoryDetailsView(LoginRequiredMixin, DetailView):
+    template_name = 'category_detail.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organised:
+            queryset = Category.objects.filter(
+                organiser=user.userprofile)
+        else:
+            queryset = Category.objects.filter(
+                organiser=user.spy.organiser)
+        return queryset
