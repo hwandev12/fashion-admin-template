721f8cb071cd30300526994affb3f78782fcaef4
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-22 16:10:19 +0500

    heroku staticfiles

--- syscrm/settings.py
+++ syscrm/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+STATIC_ROOT = 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
